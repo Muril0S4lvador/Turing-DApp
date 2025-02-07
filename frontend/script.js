79bcad3ee5c71410f29943a0534d94300ed4a256
Murilo <murilo.salvador95@gmail.com>
2025-02-07 14:14:19 -0300

    feat: fazendo issue, criando vote

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,10 @@ const localBlockchainAddress = "http://127.0.0.1:8545/";
 const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const provider = new ethers.providers.JsonRpcProvider(localBlockchainAddress)
+const contractEvent = _intializeContract(provider);
 const signer = provider.getSigner();
+
+let rankingData = []
 
 async function _intializeContract(init) {
   const response = await fetch(ARTIFACT_PATH);
@@ -30,11 +33,11 @@ async function loadRanking() {
 
           // Chama a função Solidity
           const rankingString = await contract.getRanking();
-          const rankingData = parseRankingData(rankingString);
+          rankingData = parseRankingData(rankingString);
 
 
           console.log("Ranking Recebido:", rankingData);
-          updateUI(rankingData);
+          updateRankingData();
       } catch (error) {
           console.error("Erro ao carregar ranking:", error);
       }
@@ -43,7 +46,7 @@ async function loadRanking() {
   }
 }
 
-function updateUI(rankingData) {
+function updateRankingData() {
   const rankingList = document.getElementById("rankingList");
   rankingList.innerHTML = ""; // Limpa a lista antes de adicionar novos elementos
 
@@ -109,16 +112,17 @@ async function updateButtonState() {
     try {
       await contract.functions.votingOn();
     } catch (error) {
-      alert("Não foi possível chamar a função");
+      alert("Unable to change Voting status");
       console.log(error);     
     }
   } else {
     voteButton.classList.add("button-off");
     voteButton.classList.remove("button-on");
     try {
+      console.log(contract.functions);
       await contract.functions.votingOff();
     } catch (error) {
-      alert("Não foi possível chamar a função");
+      alert("Unable to change Voting status");
       console.log(error);     
     }
   }
@@ -130,7 +134,7 @@ const codenameInput = document.getElementById("codename");
 const amountInput = document.getElementById("amount");
 
 // Adiciona Listener para botão Issue Token
-issueTokenButton.addEventListener("click", () => {
+issueTokenButton.addEventListener("click", async () => {
     const codename = codenameInput.value.trim(); // Remove espaços extras
     const amount = parseFloat(amountInput.value); // Converte string para número
 
@@ -141,7 +145,46 @@ issueTokenButton.addEventListener("click", () => {
 
     codenameInput.value = amountInput.value = "";
 
-    alert(`Codename: ${codename}, Amount: ${amount}`);
+    const contract = await _intializeContract(signer);
+    try {
+      await contract.issueToken(codename, amount);
+    } catch (error) {
+        console.log("Issue Token failed. Error message:", error.message);
+    }
+});
+
+// Seleciona botão Vote
+const VoteButton = document.getElementById("button-vote");
+
+// Adiciona Listener para botão Vote
+VoteButton.addEventListener("click", async () => {
+    if(!switchInput.checked){
+      console.log("Unable to vote");
+      return;
+    }
+  
+    const codename = codenameInput.value.trim(); // Remove espaços extras
+    const amount = parseFloat(amountInput.value); // Converte string para número
+
+    if (!codename || isNaN(amount) || amount <= 0) {
+        alert("Por favor, preencha um codename válido e um amount maior que zero.");
+        return;
+    }
+
+    codenameInput.value = amountInput.value = "";
+
+    const contract = await _intializeContract(signer);
+    try {
+      await contract.vote(codename, amount);
+    } catch (error) {
+        console.log("Vote failed. Error message:", error.message);
+    }
+});
+
+contractEvent.on('IssueToken', async (to, amount) => {
+  console.log('Transfer event emitted. {to, amount}');
+  rankingData.find(item => item.codename === to).amount += amount;
+  updateRankingData();
 });
 
 /**
